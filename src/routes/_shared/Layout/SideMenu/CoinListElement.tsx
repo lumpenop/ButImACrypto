c964b0bba2ce77f5864f51d0e9a1c885e42a26a6
lumpenop <swk5373@gmail.com>
2022-06-06 22:51:46 +0900

    Refactor: map() 내부 로직 빼서 함수로 만들었음

--- src/routes/_shared/Layout/SideMenu/CoinListElement.tsx
+++ src/routes/_shared/Layout/SideMenu/CoinListElement.tsx
@@ -7,8 +7,12 @@ import React from 'react'
 interface Props {
   coinList: ICoin[]
 }
-
 const coinListElement = ({ coinList }: Props) => {
+  const checkChange = (changeData: Number) => {
+    if (changeData > 0) return styles.rises
+    if (changeData < 0) return styles.fall
+    return ''
+  }
   return (
     <ul className={styles.coinListContainer}>
       <li className={styles.coinList}>
@@ -20,18 +24,14 @@ const coinListElement = ({ coinList }: Props) => {
       </li>
       {coinList?.map((item: ICoin) => {
         const changeData = new BigNumber(item.quotes.KRW.percent_change_24h).toNumber()
-        let change = ''
-        if (changeData > 0) change = styles.rises
-        if (changeData < 0) change = styles.fall
-
         return (
           <li key={item.name} className={styles.coinList}>
             <div className={styles.coinInfo}>
               <span className={styles.coinName}>{item.name}</span>
-
-              <span className={cx(styles.coinPrice, change)}>{item.quotes.KRW.price.toFixed(2)}</span>
-
-              <span className={cx(styles.coinChange, change)}>{item.quotes.KRW.percent_change_24h.toFixed(2)}%</span>
+              <span className={cx(styles.coinPrice, checkChange(changeData))}>{item.quotes.KRW.price.toFixed(2)}</span>
+              <span className={cx(styles.coinChange, checkChange(changeData))}>
+                {item.quotes.KRW.percent_change_24h.toFixed(2)}%
+              </span>
             </div>
           </li>
         )
