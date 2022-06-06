dc479d4e5b2aed899e7c7ee103333fffa8c533cf
lumpenop <swk5373@gmail.com>
2022-06-06 22:52:32 +0900

    Feat: victory.js 튜토리얼로 차트 그려봄

--- src/routes/Chart/CryptoChart.tsx
+++ src/routes/Chart/CryptoChart.tsx
@@ -1,7 +1,23 @@
 import styles from './chart.module.scss'
 
+import { VictoryChart, VictoryBoxPlot } from 'victory'
+
+const data = [
+  { x: 1, y: [1, 2, 3, 5] },
+  { x: 2, y: [3, 2, 8, 10] },
+  { x: 3, y: [2, 8, 6, 5] },
+  { x: 4, y: [1, 3, 2, 9] },
+]
+
 const CyrptoChart = () => {
-  return <div className={styles.chart}>chart</div>
+  return (
+    <div className={styles.chart}>
+      chart
+      <VictoryChart domainPadding={20}>
+        <VictoryBoxPlot boxWidth={10} data={data} whiskerWidth={0} style={{ median: { stroke: 'white' } }} />
+      </VictoryChart>
+    </div>
+  )
 }
 
 export default CyrptoChart
