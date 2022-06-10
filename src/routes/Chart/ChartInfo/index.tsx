b30ce5876427e9ac9c649b83c752170194e1386e
lumpenop <swk5373@gmail.com>
2022-06-10 23:42:55 +0900

    Refactor: 계산식 수정

--- src/routes/Chart/ChartInfo/index.tsx
+++ src/routes/Chart/ChartInfo/index.tsx
@@ -18,20 +18,30 @@ const ChartInfo = () => {
   }
 
   if (!coinListData) return <div className={styles.chartInfo} />
-  const { coinPrice, coinPercentChange24h, coinMarketCap, coinVolume24h } = coinListData
-  const prevPrice = (Number(coinPrice) + Number(coinPrice * coinPercentChange24h)) / 100
+  const { coinPrice, coinPercentChange24h, coinMarketCap, coinVolume24h, marketCapChange24h } = coinListData
+  const prevPrice = Number(coinPrice) + Number(coinPrice * (coinPercentChange24h / 100))
   const floorPrevPrice = new BigNumber(prevPrice).toNumber().toFixed(2)
+  const marketCapGap = coinMarketCap - coinMarketCap * (marketCapChange24h / 100)
+
   return (
-    <div className={styles.chartInfo}>
-      <h4>{coinListData.name}</h4>
-      <span className={cx(styles.coinPrice, checkChange(coinPercentChange24h))}>{coinPrice}</span>
-      <span className={checkChange(coinPercentChange24h)}>{coinPercentChange24h}</span>
-      <p>
-        <span>24시간 거래량{coinVolume24h}</span>
-        <span>전일가{floorPrevPrice}</span>
-        <span className={checkChange(coinPercentChange24h)}>시총{coinMarketCap}</span>
-      </p>
-    </div>
+    <section className={styles.chartInfo}>
+      <div className={styles.infoContainer}>
+        <h1 className={styles.coinName}>{coinListData.name}</h1>
+        <span className={cx(styles.coinPrice, checkChange(coinPercentChange24h))}>{coinPrice}</span>
+        <span className={checkChange(coinPercentChange24h)}>{coinPercentChange24h}%</span>
+        <p className={styles.detailInfo}>
+          <div>
+            <span>거래량</span> {(coinVolume24h / 1000000000000).toFixed(2)}T
+          </div>
+          <div>
+            <span>전일가</span> {floorPrevPrice}
+          </div>
+          <div className={checkChange(marketCapGap)}>
+            <span>시총</span> {coinMarketCap}
+          </div>
+        </p>
+      </div>
+    </section>
   )
 }
 
