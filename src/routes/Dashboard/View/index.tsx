a2457a16709143ced6addb87f2651bf986a063c1
lumpenop <swk5373@gmail.com>
2022-06-10 23:41:13 +0900

    Feat: 캐로셀 구현 시작

--- src/routes/Dashboard/View/index.tsx
+++ src/routes/Dashboard/View/index.tsx
@@ -1,7 +1,29 @@
 import styles from './view.module.scss'
+import { cx } from 'styles'
 
+import { useState, MouseEvent } from 'react'
+
+const menuList = [1, 2, 3]
 const View = () => {
-  return <div className={styles.view}>view</div>
+  const [isClicked, setIsClicked] = useState(false)
+  const menuSliding = () => {
+    setIsClicked(true)
+  }
+  return (
+    <div className={styles.view}>
+      <ul className={cx(styles.menuListContainer, { [styles.move]: isClicked })}>
+        {menuList.map((item, idx) => {
+          return (
+            <li className={styles.menuList} key={item}>
+              <button type='button' onClick={menuSliding} value={idx}>
+                {item}
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default View
