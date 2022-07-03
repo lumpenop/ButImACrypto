30333738eab260c1b1c9db018b7b6d477073cd23
lumpenop <swk5373@gmail.com>
2022-07-04 06:40:32 +0900

    Feat: 글쓰기, 모달 닫기 기능 추가 삭제는 localstorage쪽 구현 해야함..

--- src/routes/Board/Modal/index.tsx
+++ src/routes/Board/Modal/index.tsx
@@ -2,16 +2,35 @@ import ModalPortal from './Portal'
 
 import styles from './modal.module.scss'
 
-import { useState, MouseEvent, ChangeEvent } from 'react'
+import { useState, FormEvent, ChangeEvent } from 'react'
 
 import store from 'store'
 
-const Modal = () => {
+interface Props {
+  setIsModal: Function
+  setData: Function
+  data: Board[]
+}
+interface Board {
+  subject: string
+  content: string
+}
+
+const Modal = ({ setIsModal, setData, data }: Props) => {
   const [subject, setSubject] = useState('')
   const [content, setContent] = useState('')
-  const submitClick = (event: MouseEvent<HTMLButtonElement>) => {
+  const submitClick = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    store.set('board', { subject, content })
+
+    const result = store.get('board')
+
+    if (result) {
+      store.set('board', [...result, { subject, content }])
+    } else {
+      store.set('board', [{ subject, content }])
+    }
+    let num = data.length + 1
+    setData([...data, { num, id: 'id3', subject, content, date: 'date', count: 0 }])
   }
   const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSubject(event.currentTarget.value)
@@ -20,11 +39,19 @@ const Modal = () => {
   const textareaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(event.currentTarget.value)
   }
+
+  const cencelButtonClick = () => {
+    setIsModal(false)
+  }
+
   return (
     <ModalPortal>
       <div className={styles.modalContainer}>
         <div className={styles.modal}>
-          <form>
+          <button className={styles.cancelButton} type='button' onClick={cencelButtonClick}>
+            x
+          </button>
+          <form onSubmit={submitClick}>
             <input
               onChange={inputChange}
               placeholder='제목'
@@ -32,7 +59,7 @@ const Modal = () => {
               type='text'
               value={subject}
             />
-            <button className={styles.modalSubmit} onClick={submitClick} type='submit'>
+            <button className={styles.modalSubmit} type='submit'>
               작성완료
             </button>
             <textarea onChange={textareaChange} placeholder='내용' className={styles.modalContent} value={content} />
--- src/routes/Board/index.tsx
+++ src/routes/Board/index.tsx
@@ -1,47 +1,85 @@
 import styles from './board.module.scss'
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import ModalPortal from './Modal/Portal'
 import Modal from './Modal'
+import store from 'store'
 
 const head = ['No.', '아이디', '제목', '날짜', '조회수']
 const body = [
-  { num: 1, id: 'id', subject: 'subject', date: 'date', count: 1 },
-  { num: 2, id: 'id2', subject: 'subject2', date: 'date2', count: 2 },
+  { num: 1, id: 'id', subject: 'subject', content: 'content', date: 'date', count: 1 },
+  { num: 2, id: 'id2', subject: 'subject2', content: 'content', date: 'date2', count: 2 },
 ]
+
+interface Board {
+  subject: string
+  content: string
+}
 const Board = () => {
   const [isModal, setIsModal] = useState(false)
+  const [data, setData] = useState(body)
+
+  useEffect(() => {
+    const result: Board[] = store.get('board')
+    if (!result) return
+    const arr = [...body]
+    let num = body.length + 1
+    result.forEach((item) => {
+      arr.push({ num, id: 'id3', subject: item.subject, content: item.content, date: 'date', count: 0 })
+      num += 1
+    })
+    setData(arr)
+  }, [])
+
+  const deleteItem = (index: number) => {
+    const result = data.filter((_, idx) => {
+      return idx !== index
+    })
+    store.remove('board')
+    store.set('board', result)
+    setData(result)
+  }
 
   const handleModal = () => {
     setIsModal((prev) => !prev)
   }
+
+  const tBody = useMemo(
+    () =>
+      data.map((item, index) => {
+        return (
+          <tr key={item.date + index}>
+            <td>{item.num}</td>
+            <td>{item.id}</td>
+            <td>{item.subject}</td>
+            <td>{item.date}</td>
+            <td>{item.count}</td>
+            <td>
+              <button type='button' onClick={() => deleteItem(index)}>
+                x
+              </button>
+            </td>
+          </tr>
+        )
+      }),
+    [data]
+  )
+
   return (
     <div className={styles.tableContainer}>
       <button type='button' onClick={handleModal}>
         글쓰기
       </button>
-      <ModalPortal>{isModal && <Modal />}</ModalPortal>
+      <ModalPortal>{isModal && <Modal data={data} setData={setData} setIsModal={setIsModal} />}</ModalPortal>
       <div className={styles.tableBox}>
         <table className={styles.table}>
           <thead>
             <tr>
-              {head.map((item) => {
-                return <th key={item}>{item}</th>
+              {head.map((item, index) => {
+                return <th key={item + index}>{item}</th>
               })}
             </tr>
           </thead>
-          <tbody>
-            {body.map((item) => {
-              return (
-                <tr key={item.date}>
-                  <td>{item.num}</td>
-                  <td>{item.id}</td>
-                  <td>{item.subject}</td>
-                  <td>{item.date}</td>
-                  <td>{item.count}</td>
-                </tr>
-              )
-            })}
-          </tbody>
+          <tbody>{tBody}</tbody>
         </table>
       </div>
     </div>
